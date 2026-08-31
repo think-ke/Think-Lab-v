ae0e9933a112e2fa28d022695445be9a4ad67f3c
Lauryn05 <waruingilauryn05@gmail.com>
2026-08-31 14:40:58 +0300

    URL for prod added

--- src/components/home/Newsletter.tsx
+++ src/components/home/Newsletter.tsx
@@ -10,7 +10,7 @@ const ContactUs = () => {
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
 
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
